c6aed3b11de8f7fcefad67e29b690126b1433222
Guillaume Rangheard <guillaume.rangheard@gmail.com>
2020-04-30 21:18:16 +0200

    Update bla.js

--- bla.js
+++ bla.js
@@ -19,6 +19,7 @@
 		}
 		return n;
 	};
+	$.clone=function(a){if($.isElement(a)){var r=a.cloneNode&&a.cloneNode(true);if(r&&0<r.id.length){r.id="";}return r;}};
 	$.document=D.documentElement;
 	$.each=function(a,f,c){if($.isArrayLike(a)){c=c||this;var i=-1,l=a.length;while(++i<l){if(false===f.call(c,a[i],i,a)){break;}}}};
 	$.eachKey=function(o,f,c){c=c||this;var i=-1,k=$.keys(o),l=k.length;while(++i<l){f.call(c,k[i],o[k[i]]);}};
@@ -38,25 +39,37 @@
 	$.maker=function(a,b){$.make[a]=$.isString(b)?new Function("return document.createElement(\""+b+"\");"):b;};
 	$.map=function(a,f,c){c=c||this;var i=-1,l=a.length,r=[];while(++i<l){r.push(f.call(c,a[i],i,a));}return r;};
 	
-	$.api={
-		
-		push:(function(){
-			var _p=function(e){if($.isElement(e)){this[this.length]=e;this.length++;}};
-			return function(a){
-				$.each(!$.isArrayLike(a)?[a]:a,_p,this);
-				return this;
-			};
-		})()
-		
-	};
-	
-	$.extend($.api,$.document.classList?
+	$.extend($,$.document.classList?
 	{
-		
+		addClass:function(e,c){$.isElement(e)&&e.classList.add(c);},
+		hasClass:function(e,c){$.isElement(e)&&e.classList.contains(c);},
+		removeClass:function(e,c){$.isElement(e)&&e.classList.remove(c);},
+		toggleClass:function(e,c){$.isElement(e)&&e.classList.toggle(c);}
 	}:
 	{
-		
+		addClass:function(e,c){if($.isElement(e)){var d=e.className;$.each(c.split(" "),function(v){if(d.indexOf(v)<0){d+=" "+v;}});e.className=d;}},
+		hasClass:function(e,c){if($.isElement(e)){var d=e.className;return $.all(c.split(" "),function(v){return -1<d.indexOf(v);});}return false;},
+		removeClass:function(e,c){if($.isElement(e)){e.className=$.map(e.className.split(" "),function(v){return -1<c.indexOf(v)?"":v;}).join(" ");}},
+		toggleClass:function(e,c){
+			
+		}
 	});
+	
+	$.api={
+		addClass:function(c){return this.each(function(){$.addClass(this,c);});},
+		append:function(a){},
+		appendTo:function(a){},
+		each:function(f){if(this.length){var i=-1,l=this.length;while(++i<l){if(false===f.call(this[i],i)){break;}}}return this;},
+		hasClass:function(c){return this.length?$.hasClass(this[0],c):false;},
+		prepend:function(a){},
+		prependTo:function(a){},
+		push:(function(){
+			var _p=function(e){if($.isElement(e)){this[this.length]=e;this.length++;}};
+			return function(a){$.each(!$.isArrayLike(a)?[a]:a,_p,this);return this;};
+		})(),
+		remove:function(){},
+		removeClass:function(c){return this.each(function(){$.removeClass(this,c);});}
+	};
 	
 	W.$=$;
 })(document,Object,window);
