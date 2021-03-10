0a4ffdf8d0c5cefb7521c9ee65f54108c64107f2
Guillaume Rangheard <guillaume.rangheard@gmail.com>
2021-03-10 08:30:09 +0100

    Create bla-bRect-0.1.0.js

---
+++ plugins/bla-bRect-0.1.0.js
@@ -0,0 +1,11 @@
+$.api.bRect=function(){
+	var r={x:0,y:0,width:0,height:0};
+	if(this.length){
+		var s=this[0].getBoundingClientRect();
+		r.x=s.left||0;
+		r.y=s.top||0;
+		r.width=s.width||0;
+		r.height=s.height||0;
+	}
+	return r;
+};
