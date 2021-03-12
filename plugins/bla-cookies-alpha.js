c38a43f5cfeb3cdd18f3a8fff5160f41e52bd6c6
Guillaume Rangheard <guillaume.rangheard@gmail.com>
2021-03-12 15:39:40 +0100

    Create bla-cookies-alpha.js

---
+++ plugins/bla-cookies-alpha.js
@@ -0,0 +1,10 @@
+$.cookie=function(k,v){
+    switch(arguments.length){
+        case 1:
+            // Some code here.
+            break;
+        case 2:
+             // Some code here.
+            return $;
+    }
+};
