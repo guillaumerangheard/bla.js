61a1019fef5353223636a9716b94dd17fdc2d542
Guillaume Rangheard <guillaume.rangheard@gmail.com>
2021-03-15 08:56:35 +0100

    Update bla-aspectRatio-0.1.0.js

--- plugins/bla-aspectRatio-0.1.0.js
+++ plugins/bla-aspectRatio-0.1.0.js
@@ -1,14 +1,12 @@
-// This plugin requires bla-viewport.
-
-// Number $.aspectRatio ( )
-// This function returns the aspect ratio of the viewport.
+// [0.1.0] Number $.aspectRatio ( )
+//// Requires: $.viewport
 $.aspectRatio=function(){
 	var a=$.viewport();
 	return a.width/a.right;
 };
 
 // Number $.prototype.aspectRatio ( )
-// This function returns the aspect ratio of the first element in the list.
+//// Requires: $.prototype.bRect
 $.api.aspectRatio=function(){
 	var a=this.bRect();
 	return a.width?a.width/a.height:0;
