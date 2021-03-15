2574ed10fc1b48a7505fce42fa0ee52aed86915c
Guillaume Rangheard <guillaume.rangheard@gmail.com>
2021-03-15 09:10:23 +0100

    Update bla-viewport-0.1.0.js

--- plugins/bla-viewport-0.1.0.js
+++ plugins/bla-viewport-0.1.0.js
@@ -1,13 +1,27 @@
 // This little plugin was inspired by verge.js.
-// It has three core, self-explanatory functions:
-// $.viewport ( )
-// $.viewport.width ( )
-// $.viewport.height ( )
-$.viewport=(function(D,M,W){
-	var W=function(){return M.max(D.clientWidth,W.innerWidth);},
-		H=function(){return M.max(D.clientHeight,W.innerHeight);},
-		V=function(){return {width:W(),height:H()};};
-	V.width=W;
-	V.height=H;
-	return V;
-})($.document,Math,window);
+// It has three core, self-explanatory functions.
+if($){
+	$.viewport=(function(D,M,W){
+			// [0.1.0] Number $.viewport.width ( )
+		var W=function(){
+				return M.max(D.clientWidth,W.innerWidth);
+			},
+
+			// [0.1.0] Number $.viewport.height ( )
+			H=function(){
+				return M.max(D.clientHeight,W.innerHeight);
+			},
+
+			// [0.1.0] Rectangle $.viewport ( )
+			V=function(){
+				return {
+					width:W(),
+					height:H()
+				};
+			};
+
+		V.width=W;
+		V.height=H;
+		return V;
+	})($.document,Math,window);
+}
