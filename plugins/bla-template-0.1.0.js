bac6b8d4512e3c7ec2f14c2a8705b03c9a1ad9d4
Guillaume Rangheard <guillaume.rangheard@gmail.com>
2021-03-15 09:07:19 +0100

    Update bla-template-0.1.0.js

--- plugins/bla-template-0.1.0.js
+++ plugins/bla-template-0.1.0.js
@@ -1,14 +1,24 @@
+// [alpha] $.Template ( [ String source = "" ] )
 $.Template=(function(){
 	var T=function(s){
 			if(!(this instanceof T)){
 				return new T(s);
 			}
 			this.source=""+(s||"");
+			this.render=T.compile(this.source);
 			this.compile();
 		},
 		Tp=T.prototype;
 	
-	Tp.compile=function(){
-		this.render=new Function("data","return (function(){}).call(data);");
+	// [alpha] Function $.Template.compile ( String source )
+	T.compile=function(s){
+		return new Function("data",s.length?"return (function(){}).call(data);":"return \"\";");
 	};
+	
+	// [alpha] Function $.Template.prototype.compile ( )
+	Tp.compile=function(){
+		this.render=T.compile(this.source);
+	};
+	
+	// [alpha] Boolean $.Template.prototype.render ( [ Object data = {} ] )
 })();
