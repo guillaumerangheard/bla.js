5a8ab3da5675fa4117a3b0f1944b64e5f744241f
Guillaume Rangheard <guillaume.rangheard@gmail.com>
2022-02-17 10:06:53 +0100

    Update and rename bla-plug-0.1.0.js to bla.plug.0.1.js

--- plugins/bla-plug-0.1.0.js
+++
@@ -1,9 +0,0 @@
-// [alpha] Boolean $.plug ( String name , Function code )
-//// Requires: $.isFunction , $.isString
-$.api.plug=function(a,b){
-	if($.isString(a)&&$.isFunction(b)){
-		$.prototype[a]=b;
-		return true;
-	}
-	return false;
-};
