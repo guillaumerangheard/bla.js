85dbb61dcfe7e18fdcd753f969ec7b69b0784081
Guillaume Rangheard <guillaume.rangheard@gmail.com>
2022-03-01 09:05:11 +0100

    Update and rename bla-aspectRatio-0.1.0.js to bla-aspectratio-0.1.js

--- plugins/bla-aspectratio-0.1.js
+++ plugins/bla-aspectratio-0.1.js
@@ -1,12 +1,14 @@
-// [0.1.0] Number $.aspectRatio ( )
-//// Requires: $.viewport
+// [0.1] Number $.aspectRatio ( )
+// Works with Bla 0.1+
+// Requires: $.viewport
 $.aspectRatio=function(){
 	var a=$.viewport();
 	return a.width/a.right;
 };
 
 // Number $.prototype.aspectRatio ( )
-//// Requires: $.prototype.bRect
+// Works with Bla 0.1+
+// Requires: $.prototype.bRect
 $.api.aspectRatio=function(){
 	var a=this.bRect();
 	return a.width?a.width/a.height:0;
